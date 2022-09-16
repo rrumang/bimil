fcb061f29c313c0abc45224dd557d587946ec6b1
유민하 <ymh22@angeluce.kr>
2022-09-16 17:11:53 +0900

    회원가입 및 로그인페이지 추가 및 파이어베이스연동

---
+++ src/components/Router.js
@@ -0,0 +1,19 @@
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+
+const AppRouter = ({isLoggedIn}) => {
+    return (
+        <Router>
+            <Routes>
+                {isLoggedIn ? (
+                        <Route path="/" element={<Home ß/>} />
+                ) : (
+                        <Route path="/" element={<Auth />} />
+                )}
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
