faf7b8e941c4d1beebcba5dcc4ec8afbd3dec864
유민하 <ymh22@angeluce.kr>
2022-09-18 16:42:14 +0900

    내비게이션 추가 및 로그아웃 처리하기

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,25 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({isLoggedIn}) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Routes>
                 {isLoggedIn ? (
-                        <Route path="/" element={<Home ß/>} />
+                    <>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
+                    </>
                 ) : (
-                        <Route path="/" element={<Auth />} />
+                    <Route path="/" element={<Auth />} />
                 )}
+                {/* 로그아웃 후 주소이동 방법 첫번째 : Navigate사용, 두번째 방법은 Profile.js에 사용 */}
+                {/* 잘못된 네비게이터 사용코드 : <Navigate from="*" to="/" /> */}
+                {/* <Route path="*" element={<Navigate to ="/" />}/> */}
             </Routes>
         </Router>
     );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,29 @@
-const Profile = () => <span>Profile</span>;
+import { authService } from "fbase";
+import {useNavigate} from "react-router-dom";
+
+const Profile = () => {
+    const history = useNavigate();
+
+    const onLogOutClick = () => {
+        authService.signOut();
+        // 로그아웃 후 주소이동 방법 두번째 : react-router-dom의 useNavigate를 이용한 방법
+        // v5
+        // const history = useHistory();
+        // history.push('/home');
+        // history.replace('/home');
+        
+        // v6
+        // const navigate = useNavigate();
+        // navigate('/home');
+        // navigate('/home', {replace: true});
+        history("/");
+    };
+
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+};
 
 export default Profile;
